da82e9bcc5c13e43b83d8af24563a5c5573f4219
Monika Dabrowska <monikad71@onet.pl>
2023-01-08 18:41:57 +0100

    Solution 13.3

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,6 +1,7 @@
 package pl.javastart.task;
 
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Main {
@@ -12,8 +13,19 @@ public class Main {
         List<Product> products = FileUtils.readProducts(secondFileName);
         List<Currency> currencies = FileUtils.readCurrency(firstFileName);
 
-        ProductUtils.calculateSumOfProductsEur(products);
-        System.out.println(ProductUtils.calculateSumOfProductsEur(products));
+        List<Product> productsWithEuroPrice = ProductUtils.getProductsWithEuroPrice(products, currencies);
+
+        BigDecimal sum =  ProductUtils.sumOfProductsInEuro(productsWithEuroPrice);
+        System.out.println("Suma wszystkich produktów w EUR: " + sum);
+
+        BigDecimal meanValue = ProductUtils.calculateMeanValueOfProductInEur(productsWithEuroPrice);
+        System.out.println("Średnia: " + meanValue);
+
+        BigDecimal highestNumber = ProductUtils.findHighestPriceOfProductInEur(productsWithEuroPrice);
+        System.out.println("Najwyższa cena w EUR: " + highestNumber);
+
+        BigDecimal lowestNumber = ProductUtils.findLowestPriceOfProductInEur(productsWithEuroPrice);
+        System.out.println("Najniższa cena w EUR: " + lowestNumber);
 
     }
 }
--- src/main/java/pl/javastart/task/ProductUtils.java
+++ src/main/java/pl/javastart/task/ProductUtils.java
@@ -1,32 +1,64 @@
 package pl.javastart.task;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductUtils {
-    static Currency currency;
 
-    static BigDecimal calculateSumOfProductsEur(List<Product> products) {
-        BigDecimal sum = BigDecimal.ZERO;
-        BigDecimal result = BigDecimal.ZERO;
+    static List<Product> getProductsWithEuroPrice(List<Product> products, List<Currency> currencies) {
+        List<Product> productsEuro = new ArrayList<>();
         for (Product product : products) {
-            if (product.getCurrency().equals(currency.getCountryCode())) {
-                BigDecimal number = product.getPrice();
-                BigDecimal number2 = currency.getRate();
-                result = number.divide(number2);
+            BigDecimal productPrice = product.getPrice();
+            BigDecimal currency1 = getCurrencyByCode(product.getCurrency(), currencies);
+            BigDecimal euroPrice = productPrice.divide(currency1, MathContext.DECIMAL64);
+            Product newProducts = new Product(product.getName(), euroPrice, product.getCurrency());
+            productsEuro.add(newProducts);
+        }
+        return productsEuro;
+    }
+
+    private static BigDecimal getCurrencyByCode(String currencyCode, List<Currency> currencies) {
+        for (Currency currency : currencies) {
+            if (currency.getCountryCode().equals(currencyCode)) {
+                return currency.getRate();
             }
-            sum = sum.add(result);
+        }
+        return null;
+    }
+
+    static BigDecimal sumOfProductsInEuro(List<Product> products) {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Product product : products) {
+            sum = sum.add(product.getPrice());
         }
         return sum;
     }
 
-    static void calculateMeanValueOfProductsInEur() {
+    static BigDecimal calculateMeanValueOfProductInEur(List<Product> products) {
+        BigDecimal sum = BigDecimal.ZERO;
+        int counter = 0;
+        for (Product product : products) {
+            sum = sum.add(product.getPrice());
+            counter++;
+        }
+        return sum.divide(new BigDecimal(counter), MathContext.DECIMAL64);
     }
 
-    static void findHighestPriceOfProductInEur() {
+    static BigDecimal findHighestPriceOfProductInEur(List<Product> products) {
+        BigDecimal highestNumber = BigDecimal.ZERO;
+        for (Product product : products) {
+            highestNumber = highestNumber.max(product.getPrice());
+        }
+        return highestNumber;
     }
 
-    static void findLowestPriceOfProductInEur() {
+    static BigDecimal findLowestPriceOfProductInEur(List<Product> products) {
+        for (Product product : products) {
+            BigDecimal lowestNumber1 = product.getPrice();
+            return lowestNumber1.min(product.getPrice());
+        }
+        return null;
     }
-
 }
